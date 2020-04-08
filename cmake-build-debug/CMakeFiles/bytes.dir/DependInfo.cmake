aef3ce825b9f6c1c33e89477fc9d7c501bc54872
Yuval Levy <59339661+yuvallevy135@users.noreply.github.com>
2020-04-08 13:56:54 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/bytes.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/yuvallevy/CLionProjects/bytes/bytes.c" "/home/yuvallevy/CLionProjects/bytes/cmake-build-debug/CMakeFiles/bytes.dir/bytes.c.o"
+  "/home/yuvallevy/CLionProjects/bytes/main.c" "/home/yuvallevy/CLionProjects/bytes/cmake-build-debug/CMakeFiles/bytes.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/bytes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bytes.dir/bytes.c.o"
+  "CMakeFiles/bytes.dir/main.c.o"
+  "bytes"
+  "bytes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bytes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
